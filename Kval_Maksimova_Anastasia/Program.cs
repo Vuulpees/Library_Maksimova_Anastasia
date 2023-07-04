231027f876c0b75ee34b72bd06d79cb15360dbf3
Ломакин Кирилл <imbq@gmail.com>
2023-07-04 10:51:42 +0300

    Изменение

--- Kval_Maksimova_Anastasia/Program.cs
+++ Kval_Maksimova_Anastasia/Program.cs
@@ -11,6 +11,28 @@ namespace Kval_Maksimova_Anastasia
     {
         static void Main(string[] args)
         {
+            Team team = new Team("Команда ", "ул Красная Пресня\n");
+            Player player1 = new Player("Иванов Перт Игоревич ", "Вратарь\n");
+            Console.WriteLine("---------------------------------");
+
+            Player player2 = new Player("Смолов Иван Иванович", "Нападающий\n");
+           
+
+            List<object> objects = new List<object> { team, player1, player2 };
+
+            
+            foreach (var obj in objects)
+            {
+                Console.WriteLine(obj.ToString());
+
+
+
+            }
+            Console.WriteLine("---------------------------------");
+            Console.ReadKey();
         }
-    }
-}
+}   }   
+
+        
+    
+
--- Library/Class1.cs
+++ Library/Class1.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    public class Class1
-    {
+    
+    
+        public class Team : IComparable<Team>
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+
+            public Team(string name, string address)
+            {
+                Name = name;
+                Address = address;
+            }
+
+            public int CompareTo(Team other)
+            {
+                return string.Compare(Name, other.Name);
+            }
+
+            public override string ToString()
+            {
+                return $"Команда: {Name}\nАдрес: {Address}";
+            }
+        }
+
+        public class Player : IComparable<Player>
+        {
+            public string FullName { get; set; }
+            public string Description { get; set; }
+
+            public Player(string fullName, string description)
+            {
+                FullName = fullName;
+                Description = description;
+            }
+
+            public int CompareTo(Player other)
+            {
+                return string.Compare(FullName, other.FullName);
+            }
+
+            public override string ToString()
+            {
+                return $"Игрок: {FullName}\nОписание: {Description}";
+            }
+        }
     }
-}
+
+
+
